e75d03447e13fae023a49b02994698f7f68f6f57
ZuhaNadeem <76058407+ZuhaNadeem@users.noreply.github.com>
2020-12-31 22:34:46 +0500

    added new files

---
+++ app.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+$(".slider").slick({
+   
+});
+let hamburger = document.querySelector('.hamburger');
+let MobNav = document.querySelector('.mob-nav')
+let times = document.querySelector('.times');
+hamburger.addEventListener('click',function(){
+    MobNav.classList.add('.open');
+});
+times.addEventListener('click',function(){
+
+});
+
+
+});
